1e0c43b6bf568eb8b9e7d66f61008c7ad420c985
Luiz Gustavo Ghisi <ghisiluizgustavo@gmail.com>
2023-06-21 15:17:22 -0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    //variavel do tipo mutavel
+    var nome = "Ana Paula Pasini"
+
+    // variavel do tipo imutavel
+    val idade = 28
+    println(nome)
+    println(idade)
+
+}
